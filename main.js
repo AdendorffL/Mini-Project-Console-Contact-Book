62240cb6df0fe355804329b0a97b81fb40fe2d32
Lourens Adendorff <lou.adendorff@gmail.com>
2026-02-19 13:21:30 +0200

    feature: add the function to add contacts to file

--- main.js
+++ main.js
@@ -1,23 +1,39 @@
 // import File System
 const fs = require("fs");
 
-// starting contact object
-const contacts = [
-    {
-        name: "Lourens Adendorff",
-        phone: "0823382284",
-        email: "lou.adendorff@gmail.com"
-    },
-    {
-        name: "Johan Pieterse",
-        phone: "0826241383",
-        email: "johanp@gmail.com"
-    }
+// read
+const fileData = fs.readFileSync("contacts.json", "utf-8");
 
-];
+// parse
+const contacts = JSON.parse(fileData);
 
-// convert object to string
-const jsonString = JSON.stringify(contacts, null, 2);
+// log every contact in neat format
+let displayContacts = () => {
+    contacts.forEach(contact => {
+        console.log(`Name: ${contact.name} \nPhone: ${contact.phone}\nEmail: ${contact.email}\n`);
+        }
+    );
+}
 
-// write to json file
-fs.writeFileSync("contacts.json", jsonString);
+console.log("\n---------------------------------\n")
+
+// function for adding contacts
+
+let addContact = (name, phone, email) => {
+    contacts.push({
+        name: name,
+        phone: phone,
+        email: email
+    })
+}
+
+addContact('Jane Doe', '0823218831', 'janedoe@gmail.com');
+addContact('John Doe', '0648494663', 'johndoe@gmail.com');
+
+displayContacts();
+
+// stringify 
+const updatedJson = JSON.stringify(contacts, null, 2);
+
+// write
+fs.writeFileSync("contacts.json", updatedJson);
